c5e7e5acc2f0f6736add8a2364d562cff08813bc
seyed_ruzaik_1999 <seyed.ruzaik@oasisdigitalagency.com>
2023-02-18 18:39:24 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import openai
+import urllib.request
+from PIL import Image
+import streamlit as st
+
+openai.api_key = "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"
+
+# Set page title and favicon
+st.set_page_config(page_title="DALL.E - Image Generation - OpenAI", page_icon="icon.jpeg")
+
+
+def generate_image(image_description):
+    img_response = openai.Image.create(
+        prompt=image_description,
+        n=1,
+        size="512x512")
+
+    img_url = img_response['data'][0]['url']
+
+    urllib.request.urlretrieve(img_url, 'img.png')
+
+    img = Image.open("img.png")
+
+    return img
+
+
+# page title
+st.title('DALL.E - Image Generation - OpenAI')
+
+# text input box for image recognition
+img_description = st.text_input('Image Description')
+
+if st.button('Generate Image'):
+    generated_img = generate_image(img_description)
+    st.image(generated_img)
